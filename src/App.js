b6236131bc2c9952102c3959f0f84e87c89cf371
otterlee9043 <otterlee99@gmail.com>
2022-04-03 15:27:44 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Octokit } from "@octokit/core";
 
-function App() {
+async function App() {
+  const octokit = new Octokit({
+    auth: `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`,
+  });
+
+  //get commits
+  // const response = await octokit.request(
+  //   "GET /repos/{owner}/{repo}/git/commits/{commit_sha}",
+  //   {
+  //     owner: "otterlee9043",
+  //     repo: "clipboard",
+  //     commit_sha: "45114aa3e7a9b8dd01b99ea6d940eba99ebc5149",
+  //   }
+  // );
+
+  //get a tree
+  const response2 = await octokit.request(
+    "GET /repos/{owner}/{repo}/git/trees/{tree_sha}",
+    {
+      owner: "otterlee9043",
+      repo: "clipboard",
+      tree_sha: "efd11b61478b314dbbd6c820dc2f6dec1e718d7e",
+    }
+  );
+
+  console.log(response2);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        hello
       </header>
     </div>
   );
